497c06984c5f0f6acb4995daac39d16422299a21
PietroTy <pietro.turcimm@gmail.com>
2023-05-10 18:00:15 -0300

    suporte+

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FITme1'`, () => {
+  it(`should have as title 'FITme'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FITme1');
+    expect(app.title).toEqual('FITme');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('FITme1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FITme app is running!');
   });
 });
